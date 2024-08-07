c4d3c7f0a04bd5f418caa2fcf6e043704e5b4e90
Nikolay <n.petrenko@tg.dunice.net>
2024-08-07 12:58:00 +0300

    added task buttons + logic for it, page buttons (w/o logic)

--- main.js
+++ main.js
@@ -1,206 +1,211 @@
-(() => {
+//(() => {
 
 
-    let TaskArray = [];//true is checked, false not
-    // task array data like {name:"name", status:true/false}
+let TaskArray = [];//true is checked, false not
+// task array data like {name:"name", status:true/false}
 
-    //it doesn't change status  
-    let taskContainer = document.querySelector("#task-container")
-    let completedTaskCounter = document.querySelector("#completed-tasks-count")
-    let activeTaskCounter = document.querySelector("#active-tasks-count")
-    let allTaskCounter = document.querySelector("#all-tasks-count")
-    let nameInput = document.querySelector("#nameholder")
-    let checkAllButton = document.querySelector("#check-all")
-    let submitButton = document.querySelector("#submit-key")
-    let deleteButton = null
+//it doesn't change status  
+let taskContainer = document.querySelector("#task-container")
+let pageContainer = document.querySelector("#page-container")
+let completedTaskCounter = document.querySelector("#completed-tasks-count")
+let activeTaskCounter = document.querySelector("#active-tasks-count")
+let allTaskCounter = document.querySelector("#all-tasks-count")
+let nameInput = document.querySelector("#nameholder")
+let checkAllButton = document.querySelector("#check-all")
+let submitButton = document.querySelector("#submit-key")
 
 
-    let completeTaskButton = null
 
 //working on it
-    function refreshDeleteQuery(){
-        deleteButton = document.querySelectorAll("#delbutton").forEach((element)=>{element.addEventListener(("click"))})
-    }
 
-    function refreshCompleteQuery(){
-        completeTaskButton = document.querySelectorAll(".complete-flag").forEach((element)=>{
-            element.addEventListener(("click"), (el)=>{
-                console.log("ELELLELE")
-            }
-        )
-        }
-    
-        )
-    }
-
-    //remake
-    function escapeExp(string) {
-        let isPreviousSpace = false
-        let protectedString = ""
-        for (let i = 0; i < string.length; i++)
-            if (string[i] === ">") {
-                protectedString += "&gt";
-            }
-            else if (string[i] === "<") {
-                protectedString += "&lt;";
-            }
-            else if (string[i] === " " && isPreviousSpace) {
-                isPreviousSpace = true;
-                continue;
-            }
-            else if (string[i] === " ") {
-                protectedString += " "
-                isPreviousSpace = true;
-                continue
-            }
-
-            else {
-                protectedString += string[i];
-                isPreviousSpace = false
-            }
-        return protectedString
-    }
-
-
-
-
-
-
-    function renderTask(TaskArray) {
-        let resultString = new String;
-
-        taskContainer.innerHTML = ""
-        TaskArray.forEach((item) => {
-            resultString += `<div class="task">`
-            if (item.status === true) {
-                resultString += `<input type="checkbox" class="complete-flag" checked="checked"></input><label>${item.name}</label>`
-            }
-            else {
-                resultString += `<input type="checkbox" class="complete-flag"></input><label>${item.name}</label>`
-            }
-            resultString += `<button class="delbutton");'>X</button></div>`
-
-        })
-        taskContainer.innerHTML += resultString
-
-
-    }
-
-
-    function delTask(taskName) { // don't work well 
-        //const index = TaskArray.indexOf(item);
-        //const x = TaskArray.splice(index, 2);
-        taskName = escapeExp(taskName)
-        TaskArray = TaskArray.filter((value) => {
-            if (value.name === escapeExp(taskName)) {
-                return false
-            }
-            return true
-        })
-
-        renderTask(TaskArray)
-
-    }
-
-    function counterUpdate() {
-        let completedTasks = new Number;
-        let activeTasks = new Number;
-        TaskArray.forEach((value) =>
-            value.status ? completedTasks++ : activeTasks++
-        )
-
-        completedTaskCounter.textContent = completedTasks
-        activeTaskCounter.textContent = activeTasks
-        allTaskCounter.textContent = activeTasks + completedTasks
-    }
-
-    
-    function addTask() {
-
-        let nameString = nameInput.value
-        nameString = escapeExp(nameString)
-        if (TaskArray.find((item) => {
-            return item.name === nameString
-        }) !== undefined
-        ) {
-            alert("COPY DETECTED")
-            return 0
-        }
-        console.info(`added element with name ${nameString}!!`)
-        TaskArray.push({ name: nameString, status: false })
-
-        checkAllButton.checked = false;
-
-        renderTask(TaskArray)
-        refreshCompleteQuery()
-    }
-
-    function getTaskName(element) {
-        return element.target.parentElement.children[1].textContent
-    }
-
-    
-
-
-    //Listeners
-
-    document.addEventListener("click", (element) => {
-        // part of code used for deleting tasks
-        if (element.target.className === "delbutton") {
-            let taskName = getTaskName(element)
-            console.info("element will be deleted", taskName)
-            delTask(taskName)
+function checkOneCallback(element) {
+    let taskName = getTaskName(element)
+    TaskArray.forEach((value, index) => {
+        console.log(value)
+        console.log(taskName)
+        if (value.name === taskName) {
+            TaskArray[index].status = !TaskArray[index].status
             counterUpdate()
         }
+
+
     }
     )
-
-    // completeTaskButton.addEventListener("click", (element)=>
-    //     {
-
-    //         TaskArray.forEach((value,index)=>{
-    //            if (value.name===element.target.parentElement.children[1].textContent)
-    //              { TaskArray[index].status=!(TaskArray[index].status)}
-    //         })
+}
 
 
-
-    //         counterUpdate()
-    //     } 
-    // )
-
-
-    checkAllButton.addEventListener("click", (element) => {
-        if (element.target.checked) {
-            console.info("check all", element.target)
-            TaskArray.forEach((item, index) => {
-                console.log(index)
-                TaskArray[index].status = true
-            })
+function delTaskCallback(taskName) { // don't work well 
+    TaskArray = TaskArray.filter((value) => {
+        if (value.name === taskName) {
+            return false
         }
+        return true
+    })
+
+    renderTask(TaskArray)
+    counterUpdate()
+
+}
+
+//remake
+function escapeExp(string) {
+    let isPreviousSpace = false
+    let protectedString = ""
+    for (let i = 0; i < string.length; i++)
+        if (string[i] === ">") {
+            protectedString += "&gt";
+        }
+        else if (string[i] === "<") {
+            protectedString += "&lt;";
+        }
+        else if (string[i] === " " && isPreviousSpace) {
+            isPreviousSpace = true;
+            continue;
+        }
+        else if (string[i] === " ") {
+            protectedString += " "
+            isPreviousSpace = true;
+            continue
+        }
+
         else {
-            console.info("decheck all", element.target)
-            TaskArray.forEach((item, index) => {
-                TaskArray[index].status = false
-            })
+            protectedString += string[i];
+            isPreviousSpace = false
         }
-        renderTask(TaskArray)
-        counterUpdate()
-    }
+    return protectedString
+}
+
+
+
+
+
+
+function renderTask(TaskArray, onlyActive = undefined) {
+    pageGenerate(onlyActive)
+    taskContainer.innerHTML = ""
+    TaskArray.forEach((item) => {
+        if (onlyActive === true && item.status) return
+        else if (onlyActive === false && !item.status) return
+        let task = document.createElement("div")
+        task.className = "task"
+        taskContainer.appendChild(task)
+        let input = document.createElement("input")
+        input.className = "complete-flag"
+        input.checked = item.status
+        input.type = "checkbox"
+        let taskLabel = document.createElement("label")
+        taskLabel.innerHTML = item.name
+        let delButton = document.createElement("button")
+        delButton.className = "delbutton"
+        delButton.innerHTML = "X"
+
+
+        task.appendChild(input)
+        task.appendChild(taskLabel)
+        task.appendChild(delButton)
+
+        delButton.addEventListener(("click"), (element) => {
+            delTaskCallback(getTaskName(element))
+        })
+
+        input.addEventListener(("click"), (element) => {
+            checkOneCallback(element)
+        })
+
+    })
+
+
+
+}
+
+
+
+
+function counterUpdate() {
+    let completedTasks = new Number;
+    let activeTasks = new Number;
+    let allTaskCounter = new Number;
+    TaskArray.forEach((value) =>
+        value.status ? completedTasks++ : activeTasks++
     )
 
-    submitButton.addEventListener("click", () => {
-        console.log("gejkgejegjkk")
-        addTask();
-        counterUpdate()
-    }
-    )
+    completedTaskCounter.textContent = completedTasks
+    activeTaskCounter.textContent = activeTasks
+    allTaskCounter.textContent = activeTasks + completedTasks
+    console.log("here")
+    checkAllButton.disabled = !allTaskCounter.textContent
+}
 
-    //useless
-    document.addEventListener("click", (element) => {
-        console.debug("clicked", element)
+
+function addTask() {
+
+    let nameString = nameInput.value
+    nameString = escapeExp(nameString)
+    if (TaskArray.find((item) => {
+        return item.name === nameString
+    }) !== undefined
+    ) {
+        alert("COPY DETECTED")
+        return 0
     }
-    )
-})();
+    console.info(`added element with name ${nameString}!!`)
+    TaskArray.push({ name: nameString, status: false })
+
+    checkAllButton.checked = false;
+
+    renderTask(TaskArray)
+}
+
+function pageGenerate(onlyActive = undefined){
+    pageContainer.innerHTML = ""
+    //remake !!!!!!
+    for (i=0;i<TaskArray.length;i++){
+        if (onlyActive === true && TaskArray[i].status) return
+        else if (onlyActive === false && !TaskArray[i].status) return
+        let page= document.createElement("button")
+        page.innerHTML = i
+        pageContainer.appendChild(page)
+    }
+
+}
+
+function getTaskName(element) {
+    console.log(element.target.parentElement.children[1].innerHTML)
+    return element.target.parentElement.children[1].innerHTML
+}
+
+
+checkAllButton.addEventListener("click", (element) => {
+    TaskArray.forEach((item, index) => {
+        console.log(index)
+        TaskArray[index].status = element.target.checked
+    })
+
+    renderTask(TaskArray)
+    counterUpdate()
+}
+)
+
+
+submitButton.addEventListener("click", () => {
+    addTask();
+    counterUpdate()
+}
+)
+activeTaskCounter.parentElement.addEventListener("click", () => {
+    renderTask(TaskArray, true)
+})
+completedTaskCounter.parentElement.addEventListener("click", () => {
+    renderTask(TaskArray, false)
+})
+allTaskCounter.parentElement.addEventListener("click", () => {
+    renderTask(TaskArray)
+})
+//useless
+document.addEventListener("click", (element) => {
+    console.debug("clicked", element)
+}
+)
+//})();
 
 
