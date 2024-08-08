42bbb54ee5f27347a4217c57e60e83a70b1e3db4
Nikolay <n.petrenko@tg.dunice.net>
2024-08-08 10:16:46 +0300

    Added task rename logic

--- main.js
+++ main.js
@@ -1,54 +1,45 @@
 //(() => {
 
 
-let TaskArray = [];//true is checked, false not
+//true is checked, false not
 // task array data like {name:"name", status:true/false}
-
+const DOUBLE_CLICK = 2
 //it doesn't change status  
-let taskContainer = document.querySelector("#task-container")
-let pageContainer = document.querySelector("#page-container")
-let completedTaskCounter = document.querySelector("#completed-tasks-count")
-let activeTaskCounter = document.querySelector("#active-tasks-count")
-let allTaskCounter = document.querySelector("#all-tasks-count")
-let nameInput = document.querySelector("#nameholder")
-let checkAllButton = document.querySelector("#check-all")
-let submitButton = document.querySelector("#submit-key")
+const escapeKeyCode = "Escape"
+const enterKeyCode = "Enter"
+const taskContainer = document.querySelector("#task-container")
+const pageContainer = document.querySelector("#page-container")
+const completedTaskCounter = document.querySelector("#completed-tasks-count")
+const activeTaskCounter = document.querySelector("#active-tasks-count")
+const allTaskCounter = document.querySelector("#all-tasks-count")
+const nameInput = document.querySelector("#nameholder")
+const checkAllButton = document.querySelector("#check-all")
+const submitButton = document.querySelector("#submit-key")
 
 
 
-//working on it
-
-function checkOneCallback(element) {
-    let taskName = getTaskName(element)
-    TaskArray.forEach((value, index) => {
-        console.log(value)
-        console.log(taskName)
-        if (value.name === taskName) {
-            TaskArray[index].status = !TaskArray[index].status
-            counterUpdate()
-        }
 
 
-    }
-    )
+const getTaskInput = (event) =>{
+    return event.target.parentElement.children[2]
+}
+const getTaskText = (event) =>{
+    return event.target.parentElement.children[1]
+}
+const getTaskId = (event) =>{
+    return Number(event.target.parentElement.id)
 }
 
+let taskArray = [];
+//working on it
+const deleteTask = (taskId) => {
+    taskArray = taskArray.filter((task) => task.id !== taskId)
 
-function delTaskCallback(taskName) { // don't work well 
-    TaskArray = TaskArray.filter((value) => {
-        if (value.name === taskName) {
-            return false
-        }
-        return true
-    })
-
-    renderTask(TaskArray)
-    counterUpdate()
-
+    renderTasks()
 }
 
 //remake
-function escapeExp(string) {
+let escapeExp = (string) => {
     let isPreviousSpace = false
     let protectedString = ""
     for (let i = 0; i < string.length; i++)
@@ -80,132 +71,124 @@ function escapeExp(string) {
 
 
 
-function renderTask(TaskArray, onlyActive = undefined) {
-    pageGenerate(onlyActive)
-    taskContainer.innerHTML = ""
-    TaskArray.forEach((item) => {
-        if (onlyActive === true && item.status) return
-        else if (onlyActive === false && !item.status) return
-        let task = document.createElement("div")
-        task.className = "task"
-        taskContainer.appendChild(task)
-        let input = document.createElement("input")
-        input.className = "complete-flag"
-        input.checked = item.status
-        input.type = "checkbox"
-        let taskLabel = document.createElement("label")
-        taskLabel.innerHTML = item.name
-        let delButton = document.createElement("button")
-        delButton.className = "delbutton"
-        delButton.innerHTML = "X"
+const renderTasks = () => {
+    //pageGenerate(onlyActive)
+    let renderedTasks = ""
 
+    taskArray.forEach((item) => {
+        //if (onlyActive === true && item.status) return
+        //else if (onlyActive === false && !item.status) return
+        renderedTasks += `<li id="${item.id}" class="task">
+        <input type="checkbox" class="task-checkbox" ${item.isChecked ? 'checked' : ''}>
+        <span class="task-textbox">${item.taskName}</span>
+        <input style="display:none;" class="task-input"></input>
+        <button class="task-delbutton">X</button></li>`
 
-        task.appendChild(input)
-        task.appendChild(taskLabel)
-        task.appendChild(delButton)
+    }
 
-        delButton.addEventListener(("click"), (element) => {
-            delTaskCallback(getTaskName(element))
-        })
-
-        input.addEventListener(("click"), (element) => {
-            checkOneCallback(element)
-        })
-
-    })
-
-
-
-}
-
-
-
-
-function counterUpdate() {
-    let completedTasks = new Number;
-    let activeTasks = new Number;
-    let allTaskCounter = new Number;
-    TaskArray.forEach((value) =>
-        value.status ? completedTasks++ : activeTasks++
     )
-
-    completedTaskCounter.textContent = completedTasks
-    activeTaskCounter.textContent = activeTasks
-    allTaskCounter.textContent = activeTasks + completedTasks
-    console.log("here")
-    checkAllButton.disabled = !allTaskCounter.textContent
+    taskContainer.innerHTML = renderedTasks
 }
 
-
-function addTask() {
-
-    let nameString = nameInput.value
-    nameString = escapeExp(nameString)
-    if (TaskArray.find((item) => {
-        return item.name === nameString
-    }) !== undefined
-    ) {
-        alert("COPY DETECTED")
-        return 0
-    }
+const addTask = () => {
+    let nameString = escapeExp(nameInput.value)
+    if(nameString===" " ) return
+    const newTask = { id: Date.now(), taskName: nameString, isChecked: false }
     console.info(`added element with name ${nameString}!!`)
-    TaskArray.push({ name: nameString, status: false })
-
+    taskArray.push(newTask)
+    nameInput.value = ""
     checkAllButton.checked = false;
-
-    renderTask(TaskArray)
+    
+    renderTasks()
 }
 
-function pageGenerate(onlyActive = undefined,page=0){
-    pageContainer.innerHTML = ""
-    //remake !!!!!!
-    for (i=0;i<TaskArray.length;i++){
-        if (onlyActive === true && TaskArray[i].status) return
-        else if (onlyActive === false && !TaskArray[i].status) return
-        let page= document.createElement("button")
-        page.innerHTML = i
-        pageContainer.appendChild(page)
-    }
-
-}
-
-function getTaskName(element) {
-    console.log(element.target.parentElement.children[1].innerHTML)
-    return element.target.parentElement.children[1].innerHTML
-}
-
-
-checkAllButton.addEventListener("click", (element) => {
-    TaskArray.forEach((item, index) => {
-        console.log(index)
-        TaskArray[index].status = element.target.checked
+const changeTaskText = (id,taskText) =>{
+    const formatedText= escapeExp(taskText)
+    console.log(id);
+    
+    taskArray.forEach((task,index)=>{
+        console.log(task.id);
+        //task.id === id ? task.taskName = formatedText
+        if(task.id === id){
+            
+            
+            console.log("change")
+            taskArray[index].taskName = formatedText
+        }
     })
-
-    renderTask(TaskArray)
-    counterUpdate()
 }
-)
 
 
+//checkAllButton.addEventListener("click", () => checkOneCallback)
+
+
+const handleClick = (event) => {
+    console.log(event)
+    let taskId = Number(event.target.parentElement.id)
+    if (event.target.className === "task-delbutton") {
+        //some del logic
+        console.log(event.target.className);
+
+        deleteTask(taskId);
+    }
+    if (event.target.className === "task-checkbox") {
+        taskArray.forEach((task) => {
+            if (task.id === taskId) {
+                task.isChecked = event.target.checked
+            }
+        }
+        )
+
+    }
+    if (event.detail === DOUBLE_CLICK && event.target.className === "task-textbox") {
+        let taskInputElement = getTaskInput(event)
+        event.target.style.display = "None"
+        console.log("DBLCK");
+
+        taskInputElement.style.display = ""
+        taskInputElement.focus()
+        taskInputElement.value = event.target.innerHTML}
+}
+
+const handleKeyUp = (event)=>{
+    if (event.target.className === "task-input" && event.key=== escapeKeyCode){
+        let taskTextElement = getTaskText(event)
+
+        console.log("key ",event)
+        event.target.style.display = "none"
+        taskTextElement.style.display = ""
+
+    }
+    if(event.target.className === "task-input" && event.key=== enterKeyCode){
+        let taskInputElement=event.target
+        let taskTextElement = getTaskText(event)
+        const taskId = getTaskId(event)
+        
+        changeTaskText(taskId, taskInputElement.value)
+        taskInputElement.value = ""
+        taskInputElement.style.display = "none"
+        taskTextElement.style.display = ""
+        
+        renderTasks()
+        
+    }
+    
+}
+
+
+//ul button event
+taskContainer.addEventListener("click", handleClick)
+
+
+taskContainer.addEventListener("keyup",handleKeyUp)
+
+
+
+//submit button event
 submitButton.addEventListener("click", () => {
-    addTask();
-    counterUpdate()
-}
+    addTask();}
 )
-activeTaskCounter.parentElement.addEventListener("click", () => {
-    renderTask(TaskArray, true)
-})
-completedTaskCounter.parentElement.addEventListener("click", () => {
-    renderTask(TaskArray, false)
-})
-allTaskCounter.parentElement.addEventListener("click", () => {
-    renderTask(TaskArray)
-})
-//useless
-document.addEventListener("click", (element) => {
-    console.debug("clicked", element)
-}
-)
+
 //})();
 
 
